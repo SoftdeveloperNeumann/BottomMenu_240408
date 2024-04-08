cdf70aa794c313ef150a64ca3f66b6926275ebad
Frank <f.neumann.berlin@googlemail.com>
2024-04-08 12:32:39 +0200

    Grundgeruest mit Fragments zum Fertigstellen

--- app/src/main/java/com/example/bottommenu/MainActivity.kt
+++ app/src/main/java/com/example/bottommenu/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.bottommenu
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.bottommenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomBar.setOnItemSelectedListener { item:MenuItem ->
+
+            val fragment = when(item.itemId){
+                R.id.action_home -> HomeFragment()
+
+                else -> Fragment()
+            }
+
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(binding.fragmentContainerView.id,fragment)
+            transaction.commit()
+
+            true
+        }
 
     }
 }
